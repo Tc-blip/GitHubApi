5cdf894f6e683863c47cbb528d68db697dd7488d
TC <chengtian372@gmail.com>
2020-02-16 17:51:08 -0500

    update

--- githubapi.py
+++ githubapi.py
@@ -13,7 +13,7 @@ def getRepositories(UserID):
         r2 = requests.get(url2)
         
         #print(f"Repo: {i['name']} Number of commits: {len(r2.json())}" )
-        res.append(i['name'],len(r2.json()))
+        res.append([i['name'],len(r2.json())])
     return res
 
 if __name__=="__main__":
