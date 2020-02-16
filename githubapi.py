64a708ac723649c185b7a3e504ce627c77468b17
TC <chengtian372@gmail.com>
2020-02-16 17:44:19 -0500

    Update githubapi.py

--- githubapi.py
+++ githubapi.py
@@ -6,10 +6,10 @@ import json
 
 
 def getRepositories(UserID):
-    r=requests.get(f"https://api.github.com/users/{UserID}/repos")
+    r=requests.get("https://api.github.com/users/{}/repos".format(UserID))
     res = []
     for i in r.json():
-        url2 = f"https://api.github.com/repos/{UserID}/{i['name']}/commits"
+        url2 = "https://api.github.com/repos/{}/{}/commits".format(UserID,i['name'])
         r2 = requests.get(url2)
         
         #print(f"Repo: {i['name']} Number of commits: {len(r2.json())}" )
