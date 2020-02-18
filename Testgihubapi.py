6cd67dec12ad477f88cd26a5dcc7deb5514b36f6
TC <chengtian372@gmail.com>
2020-02-18 18:43:30 -0500

    Update Testgihubapi.py

--- Testgihubapi.py
+++ Testgihubapi.py
@@ -8,6 +8,8 @@ class Testgithubapi(unittest.TestCase):
         except_out = [['Convenience-system',11],['GitHubApi',14],['Github_issue_spyder',17],['ssw540',4],['SSW555',30],['SSW567hw',16],['SSW695',24],['ssw810',2],['Triangle567',2]]
         self.assertEqual(getRepositories('Tc-blip'), except_out) 
 
+        except_out2 = [['hellogitworld', 30], ['helloworld', 6], ['Mocks', 10], ['Project1', 2], ['threads-of-life', 1]]
+        self.assertEqual(getRepositories('richkempinski'), except_out2)
 
 if __name__ == '__main__':
    
