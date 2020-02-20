f84e751a0363b0e983b564ad8bfb16cbf6c06f1b
TC <chengtian372@gmail.com>
2020-02-19 23:25:51 -0500

    Update Testgihubapi.py

--- Testgihubapi.py
+++ Testgihubapi.py
@@ -5,7 +5,7 @@ from githubapi import getRepositories
 
 class Testgithubapi(unittest.TestCase):
     def testgetRepositories(self):
-        except_out = [['Convenience-system',11],['GitHubApi',18],['Github_issue_spyder',17],['ssw540',4],['SSW555',30],['SSW567hw',16],['SSW695',24],['ssw810',2],['Triangle567',3]]
+        except_out = [['Convenience-system',11],['GitHubApi',20],['Github_issue_spyder',17],['ssw540',4],['SSW555',30],['SSW567hw',16],['SSW695',25],['ssw810',2],['Triangle567',3]]
         self.assertEqual(getRepositories('Tc-blip'), except_out) 
 
         except_out2 = [['hellogitworld', 30], ['helloworld', 6], ['Mocks', 10], ['Project1', 2], ['threads-of-life', 1]]
